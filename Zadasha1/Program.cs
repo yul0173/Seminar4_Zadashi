c8812c239ffe8d3028a474e3d1d2ce979a31e8a9
Юлия <108525379+yul0173@users.noreply.github.com>
2022-07-25 10:34:21 +0300

    Задача1

---
+++ Zadasha1/Program.cs
@@ -0,0 +1,19 @@
+﻿int Exponentiation(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    // int result = Math.Pow(numberA, numberB);
+    return result;
+}
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int exponentiation = Exponentiation(numberA, numberB);
+Console.WriteLine("Ответ: " + exponentiation);
+
