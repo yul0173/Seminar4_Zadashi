f0ed5a2979a141c17fc00acddb6357c4c1f1146a
Юлия <108525379+yul0173@users.noreply.github.com>
2022-07-25 10:36:26 +0300

    Задача2

---
+++ Zadasha2/Program.cs
@@ -0,0 +1,23 @@
+﻿int num, sum = 0, r;
+ 
+Console.WriteLine("Введите число : ");
+ 
+num = int.Parse(Console.ReadLine());
+ 
+while (num != 0)
+ 
+    {
+ 
+        r = num % 10;
+ 
+        num = num / 10;
+ 
+        sum = sum + r;
+ 
+    }
+ 
+Console.WriteLine("Сумма цифр числа : "+sum);
+ 
+Console.ReadLine();
+
+
