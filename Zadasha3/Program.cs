cae064760441e34e2e655ce1f51f946bdad1a80a
Юлия <108525379+yul0173@users.noreply.github.com>
2022-07-25 10:57:27 +0300

    Задача3

---
+++ Zadasha3/Program.cs
@@ -0,0 +1,17 @@
+﻿void PrintArry(int[] coll)
+{
+  int count = coll.Length;
+  int index = 0;
+  Console.Write("[");
+  while(index < count)
+  {
+    Console.Write(coll[index]);
+    index++;
+    if (index < count)
+    {
+      Console.Write(", ");
+    }
+  }
+  Console.Write("]");
+} 
+
